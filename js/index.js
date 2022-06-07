5522605449f84d8c7d64e03bde59b0cab8525a08
Sonali Tandon <sonali_tandon@outlook.com>
2022-06-07 15:04:27 -0700

    Updated index.js - replaced traditional DOM manipulation with jQuery commands.

--- js/index.js
+++ js/index.js
@@ -15,6 +15,5 @@ const showNav = () => {
 $(".modal").on("show.bs.modal", function (event) {
     let buttonClicked = $(event.relatedTarget);
     let buttonPrice = buttonClicked.attr("data");
-    let modalText = document.getElementById("pricevalue");
-    modalText.innerHTML = buttonPrice;
+    $("#pricevalue").html(buttonPrice);
 });
